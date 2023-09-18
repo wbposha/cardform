459d3212d51a9e4decf69697d56824fdf1601c87
wbposha <135736179+wbposha@users.noreply.github.com>
2023-09-18 15:25:00 +0400

    Update FormTest.java

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -124,8 +124,8 @@ public class FormTest {
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79005678910");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button.button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+        String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
     }    ///BUG
 
     @Test
@@ -135,8 +135,8 @@ public class FormTest {
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79005678910");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button.button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+        String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
     }    ///BUG
 
     @Test
